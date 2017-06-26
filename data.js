13a8df229bbd92a8680367a51f9db6ca9de61f3f
Artem Adamtsov <aay@mesto.ru>
2017-06-26 16:36:56 +0300

    Update date posts

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ module.exports = {
   posts: [
     {
       id: 1,
-      text: '1 запись',
+      text: 'Новый текст',
       image: {
         src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
         alt: '1 запись'
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       id: 2,
-      text: '2 запись',
+      text: 'Анонс Mobius 2017 Moscow: покорение Москвы',
       image: {
         src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
         alt: '2 запись',
@@ -32,7 +32,7 @@ module.exports = {
     },
     {
       id: 3,
-      text: '3 запись',
+      text: 'Сглаживание изображений фильтром анизотропной диффузии Перона и Малика',
       image: {
         src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
         alt: '3 запись',
@@ -43,7 +43,183 @@ module.exports = {
         author: 'Иван',
         createdAt: '2013-05-12',
         updatedAt: '2013-05-12',
-        likeCount: 20
+        likeCount: 15
+      }
+    },
+    {
+      id: 4,
+      text: 'Головная боль от использования математического софта',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 15
+      }
+    },
+    {
+      id: 5,
+      text: 'Сжатие фотографий без видимой потери качества: опыт Yelp',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 10
+      }
+    },
+    {
+      id: 6,
+      text: 'Как Pony Express удается вам доставлять',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 9
+      }
+    },
+    {
+      id: 7,
+      text: 'Еще один пример синтеза асинхронных схем: VME bus controller',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 12
+      }
+    },
+    {
+      id: 8,
+      text: 'Ubuntu для мобильных устройств: посмертный анализ',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 1
+      }
+    },
+    {
+      id: 9,
+      text: 'Бизнес-планирование в игровой индустрии',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 1
+      }
+    },
+    {
+      id: 10,
+      text: 'Искусственный интеллект Half-Life SDK: ретроспектива',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 1
+      }
+    },
+    {
+      id: 11,
+      text: 'Новый текст',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 1
+      }
+    },
+    {
+      id: 12,
+      text: 'Новый текст 2',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 1
+      }
+    },
+    {
+      id: 12,
+      text: 'Новый текст 3',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 1
+      }
+    },
+    {
+      id: 13,
+      text: 'Новый текст 4',
+      image: {
+        src: 'https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300',
+        alt: '3 запись',
+        width: 100,
+        height: 100
+      },
+      meta: {
+        author: 'Иван',
+        createdAt: '2013-05-12',
+        updatedAt: '2013-05-12',
+        likeCount: 1
       }
     }
   ]
